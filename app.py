db67feabaad172f5c2683d99b75bddd7dedd3352
John <johnopana2016@gmail.com>
2020-01-10 23:34:49 +0300

    added requirements file

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thisissecret!'
+
+class LoginForm(FlaskForm):
+  username = StringField('username')
+  password = PasswordField('password')
+
+
+@app.route('/form',["GET","POST"])
+def form():
+  form = LoginForm()
+  return  render_template('form.html',form=form)
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
 
 
